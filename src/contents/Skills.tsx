a481bbbac73c38d593fdfa05e21d28a8768b56ce
Rayounette <murielarisoaran@gmail.com>
2023-07-15 15:29:00 +0300

    lost ee

--- src/contents/Skills.tsx
+++ src/contents/Skills.tsx
@@ -1,5 +1,15 @@
 export default function Skills() {
     return (
-        <div className="h-screen px-[10%] py-[5%]">Skills</div>
+        <div className="h-screen px-[10%] py-[5%]">
+            <div className="grid grid-cols-2 gap-5">
+                <div>
+                    <h1 className="text-[5em] first-letter:text-[2em] font-semibold tracking-wider text-primary">Stacks</h1>
+                </div>
+                <div>
+                    <h1 className="text-[3em] first-letter:text-[1.6em] font-semibold tracking-wider text-primary">Knowledge</h1>
+                    <h1 className="text-[3em] first-letter:text-[1.6em] font-semibold tracking-wider text-primary">Language</h1>
+                </div>
+            </div>
+        </div>
     )
 }
