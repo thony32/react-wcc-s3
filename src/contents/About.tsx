76960f5beaa8967eecc24bad729f62bd2fdd79a8
Anthony <mahefasoanyrianaanthony09@gmail.com>
2023-07-15 10:56:50 +0300

    xd

--- src/contents/About.tsx
+++ src/contents/About.tsx
@@ -1,5 +1,32 @@
 export default function About() {
-    return (
-        <div>About</div>
-    )
+  return (
+    <div className="flex flex-col items-center gap-8  mx-96 p-12 rounded-xl">
+      <h1 className="text-4xl first-letter:text-5xl first-letter:text-blue-400">About <span className="text-5xl text-blue-400">Me</span></h1>
+      <div className="border-2 border-gray-100 w-1/3"></div>
+      <div>
+        <p className="text-2xl indent-16">
+          My Name is <span className="font-bold">Muriel Arisoa</span>. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Aut excepturi omnis facere distinctio. Id numquam
+          alias voluptate, dicta quia a quidem blanditiis aut explicabo, vero
+          ducimus ea illo nostrum optio.
+        </p>
+      </div>
+      <div>
+        <p className="text-2xl indent-16">
+          My Name is Muriel Arisoa. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Aut excepturi omnis facere distinctio. Id numquam
+          alias voluptate, dicta quia a quidem blanditiis aut explicabo, vero
+          ducimus ea illo nostrum optio.
+        </p>
+      </div>
+      <div>
+        <p className="text-2xl indent-16">
+          My Name is Muriel Arisoa. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Aut excepturi omnis facere distinctio. Id numquam
+          alias voluptate, dicta quia a quidem blanditiis aut explicabo, vero
+          ducimus ea illo nostrum optio.
+        </p>
+      </div>
+    </div>
+  );
 }
