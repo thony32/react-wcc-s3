0d803cba6c7da085b2ea2f7a56ea0af797b32b25
muriel <murielarisoaran@gmail.com>
2023-07-15 15:42:12 +0300

    graduation

--- src/contents/Graduation.tsx
+++ src/contents/Graduation.tsx
@@ -1,89 +1,72 @@
 export default function Graduation() {
     return (
         <div>
-            <div className="min-h-screen py-6 flex flex-col justify-center sm:py-12">
-  <div className="py-3 sm:max-w-xl sm:mx-auto w-full px-2 sm:px-0">
+           <section>
+    <div className="py-8">
+    <div className="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
+      <div className="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
+        <p className=" text-primary  font-bold text-5xl first-letter:text-7xl">Graduation</p>
+        <p className="text-xl leading-normal md:leading-relaxed mb-2">Working Process of Fest</p>
+        <p className="text-sm md:text-base mb-4">
+          Here’s your guide to the tech fest 2021 process. Go through all the steps to know the exact process of the
+          fest.
+        </p>
+      </div>
+      <div className="ml-0 md:ml-12 lg:w-2/3 sticky">
+        <div className="container mx-auto w-full h-full">
+          <div className="relative wrap overflow-hidden p-10 h-full"> 
+            <div className="border-2-2 border-yellow-555 absolute h-full border right-[50%] rounded-sm border-primary"></div>
+            <div className="border-2-2 border-yellow-555 absolute h-full border left-[50%] rounded-sm border-primary"></div>
+            <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="order-1 w-5/12 px-1 py-4 text-right">
+                <p className="mb-3 font-bold text-primary">2018 - 2019</p>
+                <h4 className="mb-3 font-bold text-lg md:text-2xl">LSJC, Fianarantsoa</h4>
+                <p className="text-sm md:text-base">
+                    Baccalaureate , SERIE C
+                </p>
+              </div>
+            </div>
+            <div className="mb-8 flex justify-between items-center w-full right-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="order-1  w-5/12 px-1 py-4 text-left">
+                <p className="mb-3 font-bold text-primary">2020 - 2021</p>
+                <h4 className="mb-3 font-bold text-lg md:text-2xl">ENI, Fianarantsoa</h4>
+                <h4 className="mb-3 font-bold text-lg md:text-2xl"></h4>
+                <p className="text-sm md:text-base ">
+                First year of bachelor’s degree in computer science
+                </p>
+              </div>
+            </div>
+            <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+              <div className="order-1 w-5/12"></div>
+              <div className="order-1 w-5/12 px-1 py-4 text-right">
+                <p className="mb-3 font-bold text-primary">2021 - 2022</p>
+                <h4 className="mb-3 font-bold text-lg md:text-2xl">ENI, Fianarantsoa</h4>
+                <p className="text-sm md:text-base ">
+                Second year of bachelor’s degree in computer science
+                </p>
+              </div>
+            </div>
 
-    <div className="relative text-gray-700 antialiased text-sm font-semibold">
+            <div className="mb-8 flex justify-between items-center w-full right-timeline">
+              <div className="order-1 w-5/12"></div>
 
+              <div className="order-1  w-5/12 px-1 py-4">
+                <p className="mb-3 font-bold text-primary">2022 - Nowadays</p>
+                <h4 className="mb-3 font-bold  text-lg md:text-2xl text-left">ENI, Fianarantsoa</h4>
+                <p className="text-sm md:text-base">
+                Third year of bachelor’s degree in computer science
 
-      <div className="hidden sm:block w-1 bg-blue-300 absolute h-full left-1/2 transform -translate-x-1/2"></div>
-
-      <div className="mt-6 sm:mt-0 sm:mb-12">
-        <div className="flex flex-col sm:flex-row items-center">
-          <div className="flex justify-start w-full mx-auto items-center">
-            <div className="w-full sm:w-1/2 sm:pr-8">
-              <div className="p-4 bg-white rounded shadow">
-                Now this is a story all about how,
+                </p>
               </div>
             </div>
           </div>
-          <div className="rounded-full bg-blue-500 border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-            </svg>
-          </div>
         </div>
       </div>
-
-      <div className="mt-6 sm:mt-0 sm:mb-12">
-        <div className="flex flex-col sm:flex-row items-center">
-          <div className="flex justify-end w-full mx-auto items-center">
-            <div className="w-full sm:w-1/2 sm:pl-8">
-              <div className="p-4 bg-white rounded shadow">
-                My life got flipped turned upside down,
-              </div>
-            </div>
-          </div>
-          <div className="rounded-full bg-blue-500 border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-            </svg>
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-6 sm:mt-0 sm:mb-12">
-        <div className="flex flex-col sm:flex-row items-center">
-          <div className="flex justify-start w-full mx-auto items-center">
-            <div className="w-full sm:w-1/2 sm:pr-8">
-              <div className="p-4 bg-white rounded shadow">
-                And I'd like to take a minute, just sit right there,
-              </div>
-            </div>
-          </div>
-          <div className="rounded-full bg-blue-500 border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-          </div>
-        </div>
-      </div>
-
-
-      <div className="mt-6 sm:mt-0">
-        <div className="flex flex-col sm:flex-row items-center">
-          <div className="flex justify-end w-full mx-auto items-center">
-            <div className="w-full sm:w-1/2 sm:pl-8">
-              <div className="p-4 bg-white rounded shadow">
-                I'll tell you how I became the Prince of a town called Bel Air.
-              </div>
-            </div>
-          </div>
-          <div className="rounded-full bg-blue-500 border-white border-4 w-8 h-8 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-            </svg>
-          </div>
-        </div>
-      </div>
-
-
-
     </div>
-
   </div>
-</div>
+  </section>
         </div>
     )
 }
