2ad2a52d65b0b2d9bbb88b22c65a540cd75791c7
Rayounette <murielarisoaran@gmail.com>
2023-07-15 15:30:04 +0300

    lost ee

--- src/contents/Heading.tsx
+++ src/contents/Heading.tsx
@@ -17,7 +17,7 @@ export default function Heading() {
   }, []);
   return (
     <div className="h-screen relative">
-      <div className="fixed top-0 left-0 w-full flex justify-center">
+      <div className="fixed top-0 left-0 w-full flex justify-center z-0">
         <img className={scrollTop > 200 ? "w-[25%] duration-1000 opacity-10 rounded-full shadow-lg" : "w-[40%] duration-1000 opacity-30 rounded-full shadow-lg"} src="/images/IMG_20230715_101821.png" alt="me" />
       </div>
       <Wordz />
