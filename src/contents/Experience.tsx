4f09b12e506b5e38bb5a59e9672c64f3f595afde
muriel <murielarisoaran@gmail.com>
2023-07-15 23:01:00 +0300

    experience

--- src/contents/Experience.tsx
+++ src/contents/Experience.tsx
@@ -1,5 +1,110 @@
 export default function Experience() {
     return (
-        <div>Experience</div>
+        <div>
+            <div className="flex items-center justify-center">
+                <div className="grid grid-cols-1 gap-24 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
+                    <div className="relative py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+                        <div className=" text-white flex items-center absolute rounded-full py-5 px-4 shadow-xl bg-primary left-4 -top-6">
+                            2022
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-xl font-semibold my-2">Internship in Web Development</p>
+                            <p className="mb-5">
+                                - Design and Development of a Web Application for Budget Management
+                            </p>
+                            <div className="border-t-2"></div>
+
+                            <div className="flex justify-between mt-5">
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">Codeigniter 4</span>
+                                    </div>
+                                </div>
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">MySQL</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="relative py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+                        <div className=" text-white flex items-center absolute rounded-full py-5 px-4 shadow-xl bg-primary left-4 -top-6">
+                            2022
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-xl font-semibold my-2">DevHunt</p>
+                            <p className="mb-5">
+                                - Design and Development of a Web Application for Budget Management
+                            </p>
+                            <div className="border-t-2"></div>
+
+                            <div className="flex justify-between mt-5">
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">ReactJS</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="relative py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+                        <div className=" text-white flex items-center absolute rounded-full py-5 px-4 shadow-xl bg-primary left-4 -top-6">
+                            2022
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-xl font-semibold my-2">Frontend Awards</p>
+                            <p className="mb-5">
+                                - Design and Development of a Web Application for Budget Management
+                            </p>
+                            <div className="border-t-2"></div>
+
+                            <div className="flex justify-between mt-5">
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">Codeigniter 4</span>
+                                    </div>
+                                </div>
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">MySQL</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="relative py-6 px-6 rounded-3xl w-64 my-4 shadow-xl">
+                        <div className=" text-white flex items-center absolute rounded-full py-5 px-4 shadow-xl bg-primary left-4 -top-6">
+                            2023
+                        </div>
+                        <div className="mt-8">
+                            <p className="text-xl font-semibold my-2">WCC</p>
+                            <p className="mb-5">
+                                - Design and Development of a Web Application for Budget Management
+                            </p>
+                            <div className="border-t-2"></div>
+
+                            <div className="flex justify-between mt-5">
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">ReactJS</span>
+                                    </div>
+                                </div>
+                                <div className="card px-3 py-2 border-current border-[1px] hover:border-primary hover:text-primary hover:scale-110 duration-100">
+                                    <div className=" flex items-center">
+                                        <span className="text-xs font-semibold">Tailwind CSS</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
